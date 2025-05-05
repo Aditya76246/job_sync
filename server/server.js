7585888c1cf4b0b349a808f619ce5b05def2e12d
Aditya soni <123356923+Aditya76246@users.noreply.github.com>
2025-05-05 19:24:50 +0530

    api routes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+// api routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/users", require("./routes/user"))
 app.use("/api/employers", require("./routes/employer"))
